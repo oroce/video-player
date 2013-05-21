e4603e07f0cd34c8dbc2b0ca941e95f8ffecbce3
oroce <robert@oroszi.net>
2013-05-21 22:32:27 +0200

    moving to svg

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+/* jshint eqnull:true */
+var
+	max = Math.max,
+	min = Math.min;
 var VideoPlayer = Backbone.View.extend({
 	events: {
 		"click .state-button": "stateChange",
@@ -14,6 +18,19 @@ var VideoPlayer = Backbone.View.extend({
 		});
 		this.videoEl = this.$( "video" )
 			.on( "timeupdate", _.bind( this.onTimeUpdate, this ) )[0];
+		this.$( ".arc" ).on("mousemove", _.bind( this.onmousemove, this ) );
+		this._v = 0;
+		var o = this.$( "svg" ).offset();
+		this.x = o.left;
+		this.y = o.top;
+		this.w2 = 40;
+		this.v = this.cv = 0;
+		this.o = {
+			min: 0,
+			max: 100,
+			step: 1,
+			stopper: true
+		};
 	},
 
 	setStateButton: function(){
@@ -35,19 +52,71 @@ var VideoPlayer = Backbone.View.extend({
 		this.setStateButton();
 	},
 	onTimeUpdate: function(){
-		
+
 		var value = (100 / this.videoEl.duration) * this.videoEl.currentTime;
-		console.log( "timeupdate", value  );
+
 		this.knobEl
 			.val( Math.floor( value ) )
 			.trigger( "change" );
 	},
-	onmousemove: function(){
-		console.log( "move", arguments );
+	onmousemove: function( e ){
+		//console.log( "move", e, e.originalEvent );
+		var v = this.xy2val(e.pageX, e.pageY);
+		if (v == this.cv){
+			console.log("v equals cv",{
+				v: v,
+				cv: this.cv
+			});
+			return;
+		}
+
+    this.change(this._validate(v));
+    this._draw();
+	},
+	val: function( v ){
+		if( v == null ){
+			return this._v;
+		}
+		this._v = v;
+
 	},
 	onChange: function( value ){
 		var time = this.videoEl.duration * (value / 100);
 		this.videoEl.currentTime = time;
+	},
+	angleOffset: -125 * Math.PI / 180,
+	angleArc: 250 * Math.PI / 180, 
+	PI2: 2*Math.PI,
+	xy2val: function (x, y) {
+			var a, ret;
+
+			a = Math.atan2(
+									x - (this.x + this.w2),
+									- (y - this.y - this.w2)
+							) - this.angleOffset;
+
+			if(this.angleArc != this.PI2 && (a < 0) && (a > -0.5)) {
+					// if isset angleArc option, set to min if .5 under min
+					a = 0;
+			} else if (a < 0) {
+					a += this.PI2;
+			}
+
+			ret = ~~ (0.5 + (a * (this.o.max - this.o.min) / this.angleArc)) + this.o.min;
+
+			if( this.o.stopper ) ret = max(min(ret, this.o.max), this.o.min);
+
+			return ret;
+	},
+	_validate: function( v ){
+		return (~~ (((v < 0) ? -0.5 : 0.5) + (v/this.o.step))) * this.o.step;
+	},
+	change: function( v ){
+		this.cv = v;
+		this.val(v);
+	},
+	_draw: function(){
+		console.log( "draw that shit", this.val() );
 	}
 }, {
 	stateTexts: {
@@ -55,7 +124,7 @@ var VideoPlayer = Backbone.View.extend({
 		"pause": "||"
 	}
 });
-/*
+
 
 	var width = 80,
 		height = 80,
@@ -94,17 +163,17 @@ var VideoPlayer = Backbone.View.extend({
 			.enter()
 				.append("g")
 				.attr("class", "arc");
-	g.on("mousemove", function(d){
+	/*g.on("mousemove", function(d){
 		
 		var ang = d.startAngle + (d.endAngle - d.startAngle)/2; 
-    // Transformate to SVG space
-    ang = (ang - (Math.PI / 2) ) * -1;
+		// Transformate to SVG space
+		ang = (ang - (Math.PI / 2) ) * -1;
 
-    // Calculate a 10% radius displacement
-    var x = Math.cos(ang) * radius * 0.1;
-    var y = Math.sin(ang) * radius * -0.1;
-    console.log( this, d, x, y, radius );
-	});
+		// Calculate a 10% radius displacement
+		var x = Math.cos(ang) * radius * 0.1;
+		var y = Math.sin(ang) * radius * -0.1;
+		console.log( this, d, x, y, radius );
+	});*/
 	g.append("path")
 			.attr("d", arc)
 			.style("fill", function( d ){ return d.data === 80 ?  "#25272A" : "transparent"; });
@@ -138,7 +207,7 @@ var addNewPie = function(){
 };
 
 
-*/
+
 var videoPlayer = new VideoPlayer({
 		el: document.body
 	});
